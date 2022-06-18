ff290f25c40dd000ada911cf1c42696cc2173021
AN <409227823@qq.com>
2022-06-18 10:16:03 +0200

    Initialisation du serveur

---
+++ autre/bd/long_life.sql
@@ -0,0 +1,116 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : mer. 15 juin 2022 à 12:35
+-- Version du serveur :  8.0.26
+-- Version de PHP : 7.3.21
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+CREATE DATABASE IF NOT EXISTS long_life;
+USE long_life;
+--
+-- Base de données : `long_life`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `ingredient`
+--
+
+DROP TABLE IF EXISTS `ingredient`;
+CREATE TABLE IF NOT EXISTS `ingredient` (
+  `id` varchar(100) NOT NULL,
+  `nom` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `ingredient_recette`
+--
+
+DROP TABLE IF EXISTS `ingredient_recette`;
+CREATE TABLE IF NOT EXISTS `ingredient_recette` (
+  `id` varchar(100) NOT NULL,
+  `id_ingredient` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`,`id_ingredient`),
+  KEY `ingredient_recette_ingredient0_FK` (`id_ingredient`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `recette`
+--
+
+DROP TABLE IF EXISTS `recette`;
+CREATE TABLE IF NOT EXISTS `recette` (
+  `id` varchar(100) NOT NULL,
+  `nom` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `utilisateur`
+--
+
+DROP TABLE IF EXISTS `utilisateur`;
+CREATE TABLE IF NOT EXISTS `utilisateur` (
+  `id` varchar(50) NOT NULL,
+  `surnom` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `mdp` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `utilisateur_recette`
+--
+
+DROP TABLE IF EXISTS `utilisateur_recette`;
+CREATE TABLE IF NOT EXISTS `utilisateur_recette` (
+  `id` varchar(50) NOT NULL,
+  `id_recette` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`,`id_recette`),
+  KEY `utilisateur_recette_recette0_FK` (`id_recette`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `ingredient_recette`
+--
+ALTER TABLE `ingredient_recette`
+  ADD CONSTRAINT `ingredient_recette_ingredient0_FK` FOREIGN KEY (`id_ingredient`) REFERENCES `ingredient` (`id`),
+  ADD CONSTRAINT `ingredient_recette_recette_FK` FOREIGN KEY (`id`) REFERENCES `recette` (`id`);
+
+--
+-- Contraintes pour la table `utilisateur_recette`
+--
+ALTER TABLE `utilisateur_recette`
+  ADD CONSTRAINT `utilisateur_recette_recette0_FK` FOREIGN KEY (`id_recette`) REFERENCES `recette` (`id`),
+  ADD CONSTRAINT `utilisateur_recette_utilisateur_FK` FOREIGN KEY (`id`) REFERENCES `utilisateur` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
