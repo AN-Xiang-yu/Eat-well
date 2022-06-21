f0414219cf29427759ffbc8c5803a74c5c63d822
essaid3 <73823873+essaid3@users.noreply.github.com>
2022-06-21 11:06:23 +0200

    Update ingredient.js

--- server/class/ingredient.js
+++ server/class/ingredient.js
@@ -1,41 +1,39 @@
 class Ingredient {
-    constructor(sequelize) {
-        this.sequelize = sequelize
-    }
+  constructor(sequelize) {
+    this.sequelize = sequelize;
+  }
 
-    /**
+  /**
      * Description : Cette fonction permet de récupérer le nom et l'id de tous les ingrédients 
      * 
      * @return {list<Ingredient>} Une liste de recettes
-     * @author author-name(Prénom NOM) (création : ??-06-2022) (modification : ??-06-2022)
+     * @author author-name(Essaid Benamar) (création : 21-06-2022) (modification : ??-06-2022)
 
-     * @état : A FAIRE
+     * @état : fait
      */
-    async getIngredients() {
-        return this.sequelize.query(
-            `SELECT *
-                FROM ingredient
-                WHERE ......... `, {
-                // replacements: {...: ... },
-            }).catch(err => res.status(400).json({ error: err }))
-    }
+  async getIngredients() {
+    return this.sequelize
+      .query(`SELECT nom,id FROM ingredient `, {
+        // replacements: {...: ... },
+      })
+      .catch((err) => res.status(400).json({ error: err }));
+  }
 
-    /**
-     * Description : Cette fonction permet de récupérer tous les ids des ingrédients 
+  /**
+     * Description : Cette fonction permet de récupérer tous les id des ingrédients 
      * 
      * @return {list<Ingredient>} Une liste de recettes
-     * @author author-name(Prénom NOM) (création : ??-06-2022) (modification : ??-06-2022)
+     * @author author-name(Essaid Benamar) (création : 21-06-2022) (modification : ??-06-2022)
 
-     * @état : A FAIRE
+     * @état : fait
      */
-    async getIdIngredients() {
-        return this.sequelize.query(
-            `SELECT id
-                FROM ingredient
-                WHERE ......... `, {
-                // replacements: {...: ... },
-            }).catch(err => res.status(400).json({ error: err }))
-    }
+  async getIdIngredients() {
+    return this.sequelize
+      .query(`SELECT id FROM ingredient `, {
+        // replacements: {...: ... },
+      })
+      .catch((err) => res.status(400).json({ error: err }));
+  }
 }
 
 exports.default = Ingredient;
