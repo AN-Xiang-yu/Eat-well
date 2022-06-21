4905e3245c5a7b5cbd158672123c2ed6fe31701c
AN <409227823@qq.com>
2022-06-21 22:45:41 +0200

    2 nouvelles requetes à faire

--- server/class/recette.js
+++ server/class/recette.js
@@ -170,13 +170,53 @@ class Recette {
      * @author author-name(Essaid Benamar) (création : 21-06-2022) (modification : ??-06-2022)
      * @état : fait
      */
-    async getTagRecettes(id) {
+    async getRecettesParId(id) {
         return this.sequelize
             .query(
                 `SELECT *
         FROM recette
         WHERE id = :id`, {
                     replacements: { id: id },
+                }
+            )
+            .catch((err) => res.status(400).json({ error: err }));
+    }
+
+    /**
+     * Description : Cette fonction permet de récupérer la recette de recommandation
+     *
+     * @param {int} idUtilisateur id d'utilisateur
+     * @return {List<Recette>} Une liste de recettes
+     * @author author-name(Prénom NOM) (création : ??-06-2022) (modification : ??-06-2022)
+     * @état : A FAIRE
+     */
+    async getRecettesParId(id) {
+        return this.sequelize
+            .query(
+                `SELECT *
+        FROM utilisateur_recette
+        WHERE ...`, {
+                    // replacements: { id: id },
+                }
+            )
+            .catch((err) => res.status(400).json({ error: err }));
+    }
+
+    /**
+     * Description : Cette fonction permet de mettre à jour la table de utilisateur_recette
+     *
+     * @param {int} idUtilisateur id d'utilisateur
+     * @param {int} idRecette id de recette
+     * @author author-name(Prénom NOM) (création : ??-06-2022) (modification : ??-06-2022)
+     * @état : A FAIRE
+     */
+    async getRecettesParId(id) {
+        return this.sequelize
+            .query(
+                `UPDATE utilisateur_recette
+                    SET nom_colonne_1 = 'nouvelle valeur'
+                    WHERE condition`, {
+                    // replacements: { id: id },
                 }
             )
             .catch((err) => res.status(400).json({ error: err }));
