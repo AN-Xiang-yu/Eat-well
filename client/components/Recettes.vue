90fe0a0478122f71bbd81a64cfe8bb70606da58e
AN <409227823@qq.com>
2022-07-05 17:22:11 +0200

    page de recettes - animations

--- client/components/Recettes.vue
+++ client/components/Recettes.vue
@@ -5,72 +5,85 @@
         <section>
             <!-- barre de recherche par défaut -->
             <div class="mtb-inter-inner">
-                <form class="flex-nowrap a-center jc-around " >
-                    <!-- barre de recherche -->
-                    <div class="flex-85 flex-nowrap a-center jc-around pad-05r border-leger-noire ombreHover background-blanc">
-                        <input class="search-input border-no background-blanc w80" type="search" placeholder="Chercher recettes par ingrédients" v-model="title">
-                        <i class="fa-solid fa-magnifying-glass"></i>
+                <form id="recherche-ingredient" @submit.prevent>
+                    <div class="flex-nowrap a-center jc-around mtb-inter-inner" v-for="(ingredient, index) in ingredients" :key="index">
+                        <!-- barre de recherche -->
+                        <div class="flex-75 flex-nowrap a-center jc-around pad-05r border-leger-noire ombreHover background-blanc">
+                            <input class="search-input border-no background-blanc w80" type="search" placeholder="Chercher recettes par ingrédient" v-model="ingredients[index]">
+                            <i class="fa-solid fa-magnifying-glass"></i>
+                        </div>
+                        <!-- bouton plus -->
+                        <button class="button-type2 btn-circle btn-xl flex a-center jc-center" @click="ajouterNouvelIngredient(ingredients)">
+                            <i class="fa-solid fa-circle-plus"></i>
+                        </button>
+                        <button class="button-type2 btn-circle btn-xl flex a-center jc-center" @click="supprimerIngredient(ingredients, index)">
+                            <i class="fa-solid fa-circle-minus"></i>
+                        </button>
                     </div>
-                    <!-- bouton plus -->
-                    <button class="button-type2 btn-circle btn-xl flex a-center jc-center">
-                        <i class="fa-solid fa-circle-plus"></i>
-                    </button>
                 </form>
             </div>
             <!-- barre de recherche avancée -->
-            <div class="mtb-inter-inner flex direction-column a-center">
+            <div class="mtb-inter-inner">
                 <!-- activer la recherche avancée -->
                 <div class="w100 flex a-center mtb-inter-inner pad-1r">
-                    <button id="button-recherche" class="button-type2 btn-circle btn-xl flex a-center jc-center mr-05r ml-05r">
+                    <button id="button-recherche" class="button-type2 btn-circle btn-xl flex a-center jc-center mr-05r ml-05r" @click="changerAffichageRechercheAvancee()">
                         <i class="fa-solid fa-circle-chevron-right"></i>
                     </button>
                     <p><span>&nbsp&nbspRecherche avancée</span></p>
                 </div>
                 <!-- formulaire de recherche avancée -->
-                <form class="recherche-avancee-formulaire flex a-center jc-around ombreHover background-blanc border-leger-noire" >
-                    <div class="flex-100 flex-nowrap a-center jc-around mtb-inter-inner">
-                        <div class="flex-85 flex-nowrap a-center jc-around pad-05r ">
-                            <input class="search-input border-no background-blanc w80" type="search" placeholder="Ingrédient à ne pas consommer" v-model="title">
-                            <i class="fa-solid fa-magnifying-glass"></i>
+                <transition name="slide-fade">
+                    <form class="recherche-avancee-formulaire m-auto flex a-center jc-around ombreHover background-blanc border-leger-noire" @submit.prevent v-show="affichageRechercheAvancee">
+                        <div class="flex-100 flex-nowrap a-center jc-around mtb-inter-inner" v-for="(ingredient, index) in ingredientsNonConsommes" :key="index">
+                            <!-- barre de recherche -->
+                            <div class="flex-75 flex-nowrap a-center jc-around pad-05r border-leger-noire ombreHover background-blanc">
+                                <input class="search-input border-no background-blanc w80" type="search" placeholder="Ingrédient à ne pas consommer" v-model="ingredientsNonConsommes[index]">
+                                <i class="fa-solid fa-magnifying-glass"></i>
+                            </div>
+                            <!-- bouton plus -->
+                            <button class="button-type2 btn-circle btn-xl flex a-center jc-center" @click="ajouterNouvelIngredient(ingredientsNonConsommes)">
+                                <i class="fa-solid fa-circle-plus"></i>
+                            </button>
+                            <button class="button-type2 btn-circle btn-xl flex a-center jc-center" @click="supprimerIngredient(ingredientsNonConsommes, index)">
+                                <i class="fa-solid fa-circle-minus"></i>
+                            </button>
                         </div>
-                        <button class="button-type2 btn-circle btn-xl flex a-center jc-center">
-                            <i class="fa-solid fa-circle-plus"></i>
-                        </button>
-                    </div>
-                    <div class="flex-80 flex direction-column mtb-inter-inner">
-                        <div>
-                            <input id="sans-porc" name="sans-porc" type="checkbox" value="sans_porc" />
-                            <label for="sans-porc">&nbsp&nbspSans porc</label>
+                        <div class="flex-80 flex direction-column mtb-inter-inner">
+                            <div>
+                                <input id="sans-porc" name="sans-porc" type="checkbox" value="sans_porc" />
+                                <label for="sans-porc">&nbsp&nbspSans porc</label>
+                            </div>
+                            <div>
+                                <input id="vegane" name="vegane" type="checkbox" value="vegane" />
+                                <label for="vegane">&nbsp&nbspVegane</label>
+                            </div>
+                            <div>
+                                <input id="vegetarien" name="vegetarien" type="checkbox" value="vegetarien" />
+                                <label for="vegetarien">&nbsp&nbspVégétarien</label>
+                            </div>
+                            <div>
+                                <input id="sans-alcool" name="sans-alcool" type="checkbox" value="sans_alcool" />
+                                <label for="sans-alcool">&nbsp&nbspSans alcool</label>
+                            </div>
+                            <div>
+                                <input id="sans-gluten" name="sans-gluten" type="checkbox" value="sans_gluten" />
+                                <label for="sans-gluten">&nbsp&nbspSans gluten</label>
+                            </div>
                         </div>
-                        <div>
-                            <input id="vegane" name="vegane" type="checkbox" value="vegane" />
-                            <label for="vegane">&nbsp&nbspVegane</label>
+                        <div class="flex-85 flex jc-around a-center mtb-inter-inner">
+                            <p class="flex-100 spe-coleur">* Pour avoir une remarque personnelle, il faut remplir tous les informations ci-dessous : taille, poids, sexe</p>
+                            <input class="flex-25 w30 border-no border-b-bleu-gros pad-05r mtb-inter-inner" type="number" step="0.01" placeholder="* Taille">
+                            <input class="flex-25 w30 border-no border-b-bleu-gros pad-05r mtb-inter-inner" type="number" step="0.01" placeholder="* Poids">
+                            <select class="flex-25 w30 border-b-leger-noire-gros pad-1r mtb-inter-inner background-blanc" name="" id="">
+                                <option value="0">* Sexe</option>
+                                <option value="homme">Homme</option>
+                                <option value="femme">Femme</option>
+                            </select>
                         </div>
-                        <div>
-                            <input id="vegetarien" name="vegetarien" type="checkbox" value="vegetarien" />
-                            <label for="vegetarien">&nbsp&nbspVégétarien</label>
-                        </div>
-                        <div>
-                            <input id="sans-alcool" name="sans-alcool" type="checkbox" value="sans_alcool" />
-                            <label for="sans-alcool">&nbsp&nbspSans alcool</label>
-                        </div>
-                        <div>
-                            <input id="sans-gluten" name="sans-gluten" type="checkbox" value="sans_gluten" />
-                            <label for="sans-gluten">&nbsp&nbspSans gluten</label>
-                        </div>
-                    </div>
-                    <div class="flex-85 flex jc-around a-center mtb-inter-inner">
-                        <p class="flex-100 spe-coleur">* Pour avoir une remarque personnelle, il faut remplir tous les informations ci-dessous : taille, poids, sexe</p>
-                        <input class="flex-25 w30 border-no border-b-bleu-gros pad-05r mtb-inter-inner" type="number" step="0.01" placeholder="* Taille">
-                        <input class="flex-25 w30 border-no border-b-bleu-gros pad-05r mtb-inter-inner" type="number" step="0.01" placeholder="* Poids">
-                        <select class="flex-25 w30 border-b-leger-noire-gros pad-1r mtb-inter-inner background-blanc" name="" id="">
-                            <option value="0">* Sexe</option>
-                            <option value="homme">Homme</option>
-                            <option value="femme">Femme</option>
-                        </select>
-                    </div>
-                    <button class="button-type1 button-normal mtb-inter-inner">Reset</button>
-                </form>
+                        <button class="button-type1 button-normal mtb-inter-inner">Reset</button>
+                    </form>
+                </transition>
+                
             </div>
             <div class="flex jc-center">
                 <button type="button" class="button-type1 button-normal">Valider</button>
@@ -144,13 +157,32 @@ module.exports = {
     name:"Recettes",
     props: {
     },
+    components: {
+
+    },
     data () {
         return {
+            ingredients : [""],
+            ingredientsNonConsommes : [""],
+            affichageRechercheAvancee : false,
         }
     },
     async mounted () {
     },
     methods: {
+        //ajouter un nouvel ingrédient
+        ajouterNouvelIngredient(listIngredients){
+            listIngredients.push("")
+        },
+        //suppirmer l'ingrédient choisi
+        supprimerIngredient(listIngredients, index){
+            if(index != 0) listIngredients.splice(index, 1)
+            else this.$set(listIngredients, index, "")
+        },
+        //changer l'état de recherche avancée
+        changerAffichageRechercheAvancee(){
+            this.affichageRechercheAvancee = !this.affichageRechercheAvancee
+        }
     }
 }
 </script>
